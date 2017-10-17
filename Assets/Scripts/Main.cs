5b3bad316cb03c35ee8782e67eb54436f4885f40
RupioNotePC <ruxtupikun@gmail.com>
2017-10-17 21:58:54 +0900

    Main.csを作って「うさぎ3Dプロジェクト作成中。」という文をConsoleに表示する文を記述した。

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("うさぎ3Dプロジェクト作成中。");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
